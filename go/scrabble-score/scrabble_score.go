0b9314df4a6567fe324281e0f3a3c4992e60026c
Stefan Langenbach <stefan@langenbach.online>
2023-02-13 22:36:30 +0000

    Add solution for Scrabble score

---
+++ go/scrabble-score/scrabble_score.go
@@ -0,0 +1,43 @@
+package scrabble
+
+import "strings"
+
+// Calculate Scrabble score for a word
+func Score(word string) int {
+	var score int
+
+	LetterValues := map[string]int{
+		"a": 1,
+		"b": 3,
+		"c": 3,
+		"d": 2,
+		"e": 1,
+		"f": 4,
+		"g": 2,
+		"h": 4,
+		"i": 1,
+		"j": 8,
+		"k": 5,
+		"l": 1,
+		"m": 3,
+		"n": 1,
+		"o": 1,
+		"p": 3,
+		"q": 10,
+		"r": 1,
+		"s": 1,
+		"t": 1,
+		"u": 1,
+		"v": 4,
+		"w": 4,
+		"x": 8,
+		"y": 4,
+		"z": 10,
+	}
+
+	for _, char := range strings.ToLower(word) {
+		score += LetterValues[string(char)]
+	}
+
+	return score
+}
